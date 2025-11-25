ed3a886d6843a9d9f42faf8b9290f27e8b3110f7
yash rana <2200520100072@gmail.com>
2025-11-26 00:50:22 +0530

    profile

--- client/src/components/ProfilePage/ProfilePage.tsx
+++ client/src/components/ProfilePage/ProfilePage.tsx
@@ -95,8 +95,8 @@ function ProfilePage({ userType }: ProfileProps) {
   });
 
   useEffect(() => {
-    fetchProfile();
-  }, [fetchProfile]);
+    fetchProfile(userType);
+  }, [fetchProfile, userType]);
 
   useEffect(() => {
     if (user) {
